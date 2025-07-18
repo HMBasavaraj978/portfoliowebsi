edc8b71873c21381646db6b1d1ccc6b63737d75c
HMBasavaraj978 <hmbasavaraj978@gmail.com>
2025-07-18 11:59:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function getText() {
-  alert("Thank you ! visite Again")
-}
+window.addEventListener("load", () => {
+  const width = window.innerWidth;
+  if (width < 600) {
+    console.log("You are on a mobile device.");
+  } else if (width < 1024) {
+    console.log("You are on a tablet.");
+  } else {
+    console.log("You are on a desktop.");
+  }
+});
